a1437d88fe36c46915b4a8257a9d282b050267e7
galvaknin10 <gal9846@gmail.com>
2025-03-18 13:02:07 +0200

    sync cube simulation with database to retrieve solution path gracefully

--- backend/app/services/solve.py
+++ backend/app/services/solve.py
@@ -1,7 +1,7 @@
 from .load_data import TREE_DATA  
 from .pocket_cube import Pocket_Cube  
 
-def solve_cube(user_state: str):
+def solve_cube(curr_state: str):
     """Finds the solution path by backtracking from the given state."""
 
     # print(f"THREE DATA: {list(TREE_DATA.keys())[:5]}")
@@ -14,28 +14,9 @@ def solve_cube(user_state: str):
     # else: 
     #     print("NOOOOO")
 
-    cube = Pocket_Cube()
-    cube.update_faces_by_symmetry(user_state)
-
-    # print(f"update_cube: {user_cube.hash_rep()}")
-
-    document = None  # Initialize variable to hold the matching dictionary entry
-
-    possible_rotations = cube.normalize_cube_by_symmetry()
-
-
-    # print("possible rotation")
-
-    for rotation in possible_rotations:
-        if rotation in TREE_DATA:
-            document = TREE_DATA.get(rotation)
-            break
-
-    if document is None: 
-        return
-
-    curr_state = document.get("state")
-
+    print(f"CURR STATE: {curr_state}")
+    document = TREE_DATA.get(curr_state)
+    print(document)
     soloution_state = "BBBBGGGGOOOORRRRWWWWYYYY"
 
     # print("user cube: ", user_cube.hash_rep())
